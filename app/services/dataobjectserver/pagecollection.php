501380ea885fd6b62cda05f69dd1b40de30bd51f
Peter G.A. Barraud <barraud@adobe.com>
2016-11-16 08:57:28 +0530

    filter pages collection for markedfordelete

--- app/services/dataobjectserver/pagecollection.php
+++ app/services/dataobjectserver/pagecollection.php
@@ -1,24 +1,17 @@
 <?php
 require_once dirname(__FILE__) . '/common/collectionbase.php';
 class pagecollection extends collectionbase {
+  // if you need to fiddle with the Items collection for a specific object,
+  // you need to override call that constructor and then make changes to $this->Items
   public function __construct($classAttrValuePairs=null,$sortby=null,$getrelatedobjectcollection = 0){
       parent::__construct($classAttrValuePairs,$sortby,$getrelatedobjectcollection);
       $pages = array();
       foreach ($this->Items as $item){
-        if ($item->id == 2){
+        if ($item->markfordelete != 1){
           array_push($pages,$item);
         }
       }
       $this->Items = $pages;
-      // $pages = array();
-  		// $logger = new Logger('getpagelist');
-  		// foreach ($items as $item){
-  		// 	$logger->write($item);
-  		// 	if ($item->markfordelete != 1){
-  		// 		array_push($pages,$item);
-  		// 	}
-  		// }
-      // $this->Items
   }
 }
 ?>
